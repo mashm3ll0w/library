b23bb467e74ba45a46e619935f0814b24ea5a300
Charles Swaleh <charlswall@live.com>
2020-05-22 00:44:54 +0300

    able to capture all the form data

--- index.js
+++ index.js
@@ -20,3 +20,40 @@ window.addEventListener("click", (e) => {
 		modal.style.display = "none";
 	}
 });
+
+//  Library
+// Global Variables
+let myLibrary = [];
+let titleField = document.getElementById("title");
+let authorField = document.getElementById("author");
+let numOfPages = document.getElementById("pages");
+let newBookBtn = document.querySelector(".addToLib");
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = function () {
+		if (read) {
+			return true;
+		}
+	};
+}
+
+function addBookToLibrary() {
+	// Get the title, author, pages and read status
+	let bookTitle = titleField.value;
+	let bookAuthor = authorField.value;
+	let bookPages = numOfPages.value;
+	console.log(bookTitle, bookAuthor, bookPages);
+}
+
+newBookBtn.addEventListener("click", () => {
+	event.preventDefault();
+	let bookTitle = titleField.value;
+	let bookAuthor = authorField.value;
+	let bookPages = numOfPages.value;
+	let readStatus = document.querySelector("input[type=radio]:checked").value;
+	console.log(bookTitle, bookAuthor, bookPages, readStatus);
+	modal.style.display = "none";
+});
