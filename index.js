01be6df02f4a520c2aefc01fd824d947c2926a25
Charles Swaleh <charlswall@live.com>
2020-05-23 00:42:27 +0300

    add toggle read status button

--- index.js
+++ index.js
@@ -27,24 +27,18 @@ let myLibrary = [
 		title: "killing floor",
 		author: "lee child",
 		pages: 473,
-		haveRead: "no",
+		haveRead: "yes",
 	},
 	{
 		title: "stealing worlds",
 		author: "karl schroeder",
 		pages: 430,
-		haveRead: "yes",
+		haveRead: "no",
 	},
 	{
 		title: "the wandering earth",
 		author: "cixin liu",
 		pages: 781,
-		haveRead: "no",
-	},
-	{
-		title: "this mortal coil",
-		author: "emily suvada",
-		pages: 586,
 		haveRead: "yes",
 	},
 ];
@@ -58,7 +52,6 @@ let libraryContainer = document.querySelector(".library");
 
 // add default books on page load
 let myBook = myLibrary.map(function (book) {
-	
 	// individual book container
 	let bookContainer = document.createElement("div");
 	bookContainer.classList.add("book");
@@ -70,10 +63,17 @@ let myBook = myLibrary.map(function (book) {
 	let bookAuthor = document.createElement("p");
 	bookAuthor.classList.add("book-author");
 	bookAuthor.innerHTML = `${book.author} - ${book.pages} pages`;
-	//read and delete buttons
+	//read button
 	let read = document.createElement("button");
 	read.classList.add("readBtn");
-	read.innerHTML = "Read";
+	// check of the book is read or not
+	if (book.haveRead === "yes") {
+		read.innerHTML = "Not Read";
+		bookContainer.classList.add("read");
+	} else {
+		read.innerHTML = "Read";
+	}
+	// delete button
 	let deleteBtn = document.createElement("button");
 	deleteBtn.classList.add("remove-book");
 	deleteBtn.innerHTML = "Delete";
@@ -92,8 +92,10 @@ function Book(title, author, pages, read) {
 	this.author = author;
 	this.pages = pages;
 	this.read = function () {
-		if (read) {
-			return true;
+		if (read === "yes") {
+			return "Not Read";
+		} else {
+			return "Read";
 		}
 	};
 }
@@ -108,9 +110,9 @@ newBookBtn.addEventListener("click", () => {
 		haveRead: document.querySelector("input[type=radio]:checked").value,
 	};
 	myLibrary.push(newBook);
-	console.table(myLibrary);
 	modal.style.display = "none";
 	addBookToLibrary();
+	toggleRead();
 });
 
 // show the book in the library
@@ -128,9 +130,15 @@ function addBookToLibrary() {
 	bookAuthor.innerHTML = `${authorField.value} - ${numOfPages.value} pages`;
 
 	//read and delete buttons
-	let read = document.createElement("button");
-	read.classList.add("readBtn");
-	read.innerHTML = "Read";
+	let readBtn = document.createElement("button");
+	readBtn.classList.add("readBtn");
+	if (document.querySelector("input[type=radio]:checked").value === "yes") {
+		readBtn.innerHTML = "Not Read";
+		bookContainer.classList.add("read");
+	} else {
+		readBtn.innerHTML = "Read";
+	}
+	// ev
 	let deleteBtn = document.createElement("button");
 	deleteBtn.classList.add("remove-book");
 	deleteBtn.innerHTML = "Delete";
@@ -138,7 +146,34 @@ function addBookToLibrary() {
 	//append to library
 	bookContainer.appendChild(bookTitle);
 	bookContainer.appendChild(bookAuthor);
-	bookContainer.appendChild(read);
+	bookContainer.appendChild(readBtn);
 	bookContainer.appendChild(deleteBtn);
 	libraryContainer.appendChild(bookContainer);
 }
+
+function toggleRead() {
+	updateReadBtns();
+	let readBtn = document.querySelectorAll(".readBtn");
+	let readStatus = document.querySelectorAll(".book");
+	for (let i = 0; i < readBtn.length; i++) {
+		readBtn[i].addEventListener("click", () => {
+			if (readBtn[i].textContent === "Read") {
+				readBtn[i].innerHTML = "Not Read";
+				if (!readStatus[i].classList.contains("read")) {
+					readStatus[i].classList.add("read");
+				}
+			} else if (readBtn[i].textContent === "Not Read") {
+				if (readStatus[i].classList.contains("read")) {
+					readStatus[i].classList.remove("read");
+				}
+				readBtn[i].innerHTML = "Read";
+			}
+		});
+	}
+}
+
+function updateReadBtns() {
+	document.querySelectorAll(".readBtn");
+}
+
+toggleRead();
