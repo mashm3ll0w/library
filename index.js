bed0f13b852ecea865cb3467eef7d93ded6af2fa
Charles Swaleh <charlswall@live.com>
2020-05-23 23:04:54 +0300

    refactor part 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 
-// Modal Controllers
+// ---- Modal Controllers --------------
 let modal = document.getElementById("myModal");
 let modalBtn = document.querySelector(".addBtn");
 let closeBtn = document.getElementsByClassName("close")[0];
@@ -20,6 +20,24 @@ window.addEventListener("click", (e) => {
 		modal.style.display = "none";
 	}
 });
+
+// ------- global variables ----------------
+let addBookToLibBtn = document.querySelector(".addToLib");
+let titleField = document.getElementById("title");
+let authorField = document.getElementById("author");
+let numOfPages = document.getElementById("pages");
+let ifRead = document.querySelector("input[type=radio]:checked");
+let libraryContainer = document.querySelector(".library");
+
+// constructor function
+function Book(title, author, pages, haveRead) {
+	{
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.haveRead = haveRead;
+	}
+}
 
 //  Default Library
 let myLibrary = [
@@ -43,92 +61,29 @@ let myLibrary = [
 	},
 ];
 
-// global variables
-let newBookBtn = document.querySelector(".addToLib");
-let titleField = document.getElementById("title");
-let authorField = document.getElementById("author");
-let numOfPages = document.getElementById("pages");
-let libraryContainer = document.querySelector(".library");
-
-// add default books on page load
-let myBook = myLibrary.map(function (book) {
-	// individual book container
-	let bookContainer = document.createElement("div");
-	bookContainer.classList.add("book");
-	// title
-	let bookTitle = document.createElement("p");
-	bookTitle.classList.add("book-title");
-	bookTitle.innerHTML = book.title;
-	// author + pages
-	let bookAuthor = document.createElement("p");
-	bookAuthor.classList.add("book-author");
-	bookAuthor.innerHTML = `${book.author} - ${book.pages} pages`;
-	//read button
-	let read = document.createElement("button");
-	read.classList.add("readBtn");
-	// check of the book is read or not
-	if (book.haveRead === "yes") {
-		read.innerHTML = "Not Read";
-		bookContainer.classList.add("read");
-	} else {
-		read.innerHTML = "Read";
-	}
-	// delete button
-	let deleteBtn = document.createElement("button");
-	deleteBtn.classList.add("remove-book");
-	deleteBtn.innerHTML = "Delete";
-
-	//append to library
-	bookContainer.appendChild(bookTitle);
-	bookContainer.appendChild(bookAuthor);
-	bookContainer.appendChild(read);
-	bookContainer.appendChild(deleteBtn);
-	libraryContainer.appendChild(bookContainer);
-});
-
-// constructor function
-function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = function () {
-		if (read === "yes") {
-			return "Not Read";
-		} else {
-			return "Read";
-		}
-	};
-}
-
-// capture the new books the user enters
-newBookBtn.addEventListener("click", () => {
+// User Entered Books
+addBookToLibBtn.addEventListener("click", () => {
 	event.preventDefault();
-	let newBook = {
-		title: titleField.value,
-		author: authorField.value,
-		pages: parseInt(numOfPages.value),
-		haveRead: document.querySelector("input[type=radio]:checked").value,
-	};
-	myLibrary.push(newBook);
+	updateReadBtns();
+	let newBook = JSON.stringify(new Book(titleField.value, authorField.value, numOfPages.value, document.querySelector("input[type=radio]:checked").value));
+	myLibrary.push(JSON.parse(newBook));
+	console.table(myLibrary);
 	modal.style.display = "none";
 	addBookToLibrary();
 	toggleRead();
 });
 
-// show the book in the library
+// Show in Library
 function addBookToLibrary() {
-	// individual book container
+	//title & author & pages
 	let bookContainer = document.createElement("div");
 	bookContainer.classList.add("book");
-	// title
 	let bookTitle = document.createElement("p");
 	bookTitle.classList.add("book-title");
 	bookTitle.innerHTML = titleField.value;
-	// author + pages
 	let bookAuthor = document.createElement("p");
 	bookAuthor.classList.add("book-author");
 	bookAuthor.innerHTML = `${authorField.value} - ${numOfPages.value} pages`;
-
 	//read and delete buttons
 	let readBtn = document.createElement("button");
 	readBtn.classList.add("readBtn");
@@ -138,11 +93,9 @@ function addBookToLibrary() {
 	} else {
 		readBtn.innerHTML = "Read";
 	}
-	// ev
 	let deleteBtn = document.createElement("button");
 	deleteBtn.classList.add("remove-book");
 	deleteBtn.innerHTML = "Delete";
-
 	//append to library
 	bookContainer.appendChild(bookTitle);
 	bookContainer.appendChild(bookAuthor);
@@ -151,22 +104,56 @@ function addBookToLibrary() {
 	libraryContainer.appendChild(bookContainer);
 }
 
+function defaultBooks() {
+	let myBook = myLibrary.map(function (book) {
+		let bookContainer = document.createElement("div");
+		bookContainer.classList.add("book");
+		// title & author & pages
+		let bookTitle = document.createElement("p");
+		bookTitle.classList.add("book-title");
+		bookTitle.innerHTML = book.title;
+		let bookAuthor = document.createElement("p");
+		bookAuthor.classList.add("book-author");
+		bookAuthor.innerHTML = `${book.author} - ${book.pages} pages`;
+		//read and delete buttons
+		let read = document.createElement("button");
+		read.classList.add("readBtn");
+		// check of the book is read or not
+		if (book.haveRead === "yes") {
+			read.innerHTML = "Not Read";
+			bookContainer.classList.add("read");
+		} else {
+			read.innerHTML = "Read";
+		}
+		let deleteBtn = document.createElement("button");
+		deleteBtn.classList.add("remove-book");
+		deleteBtn.innerHTML = "Delete";
+
+		//append to library
+		bookContainer.appendChild(bookTitle);
+		bookContainer.appendChild(bookAuthor);
+		bookContainer.appendChild(read);
+		bookContainer.appendChild(deleteBtn);
+		libraryContainer.appendChild(bookContainer);
+	});
+}
+
 function toggleRead() {
-	updateReadBtns();
+	document.querySelectorAll(".readBtn");
 	let readBtn = document.querySelectorAll(".readBtn");
 	let readStatus = document.querySelectorAll(".book");
 	for (let i = 0; i < readBtn.length; i++) {
 		readBtn[i].addEventListener("click", () => {
 			if (readBtn[i].textContent === "Read") {
 				readBtn[i].innerHTML = "Not Read";
-				if (!readStatus[i].classList.contains("read")) {
+				if (readStatus[i].classList.contains("read") === false) {
 					readStatus[i].classList.add("read");
 				}
 			} else if (readBtn[i].textContent === "Not Read") {
-				if (readStatus[i].classList.contains("read")) {
+				readBtn[i].innerHTML = "Read";
+				if (readStatus[i].classList.contains("read") === true) {
 					readStatus[i].classList.remove("read");
 				}
-				readBtn[i].innerHTML = "Read";
 			}
 		});
 	}
@@ -176,4 +163,6 @@ function updateReadBtns() {
 	document.querySelectorAll(".readBtn");
 }
 
+defaultBooks();
 toggleRead();
+
